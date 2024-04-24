d9ce2ebaad8c4b2bb99e1c51e8a1ecb1eb579dc9
YoungAlien <vaniokghr2019@gmail.com>
2024-04-24 14:43:52 +0300

    Ініціалізація

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "IPZ_CW_1_3_Romanishyn_Ivan"
+include(":app")
+ 
